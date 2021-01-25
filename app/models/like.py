292c2978295de58c25bc8629723c4ff02a508ab5
Dez <dez@dezs-mbp.attlocal.net>
2021-01-25 13:37:40 -0500

    like and user models

---
+++ app/models/like.py
@@ -0,0 +1,8 @@
+from .db import db
+from datetime import datetime
+
+Like = db.Table("likes",
+    db.Model.metadata,
+    db.Column("userId", db.Integer, db.ForeignKey("users.id")),
+    db.Column("timestamp", db.DateTime, default=datetime.now())
+)
