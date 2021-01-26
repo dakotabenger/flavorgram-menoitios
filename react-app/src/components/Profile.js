8f51beb916e7176882c45bca782f08568c2c17e2
Dez <dez@dezs-mbp.attlocal.net>
2021-01-26 12:06:00 -0500

    initial branch commit

---
+++ react-app/src/components/Profile.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import FollowerNum from "./FollowerNum";
+import FollowingNum from "./FollowingNum";
+
+function Profile(props) {
+  return <h1>Hi from Profile page</h1>;
+}
+
+export default Profile;
