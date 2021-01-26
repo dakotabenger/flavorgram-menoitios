7b94d84d22d991399de1cfd916191bc2e36e7fff
Dez <dez@dezs-mbp.attlocal.net>
2021-01-26 16:17:50 -0500

    recipe model modified

---
+++ react-app/src/components/post/index.js
@@ -0,0 +1,4 @@
+import React, {useState, useEffect} from 'react';
+import {authenticate} from "../../services/auth";
+import {useParams, NavLink} from 'react-router-dom';
+import 
---
+++ react-app/src/components/post/userPost.js
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import { useHistory } from "react-router-dom";
+
+const UserPost = ({ usepost }) => {
+  const overlay = useRef(null);
+  const history = useHistory();
+  return (
+    <div
+      className="one-user-post"
+      onMouseOver={(e) => (overlay.current.style.display = "flex")}
+      onMouseLeave={(e) => (overlay.current.style.display = "none")}
+      onClick={(e) => history.push(`/posts/${usepost.id}`)}
+    >
+      <div className="post-img-holder">
+        <img alt="User Post" src={usepost.photoUrl} />
+      </div>
+      <div ref={overlay} className="post-overlay">
+        <div className="overlay-likes">
+          <i className="far fa-heart"></i>
+          {usepost.numLikes}
+        </div>
+        <div className="overlay-comments">
+          <i className="far fa-comment"></i>
+          {usepost.numComments}
+        </div>
+      </div>
+    </div>
+  );
+};
