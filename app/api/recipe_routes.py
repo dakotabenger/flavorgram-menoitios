b0ef25d84786dee481889e95ae3bc4cc8932e544
ramses <ramses.romero.jr@gmail.com>
2021-01-26 17:46:05 -0500

    started api route, thunk, and search component

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify
 from dotenv import load_dotenv
-load_dotenv
 from flask_login import login_required
 from app.config import Config
 from app.models import Recipe
+
+load_dotenv
 
 recipe_routes = Blueprint('recipes', __name__)
 
@@ -13,17 +14,24 @@ def get_recipes():
     recipes = Recipe.query.all()
     return {"recipes": [recipe.to_dict() for recipe in recipes]}
 
+
 @recipe_routes.route('/create_recipe', methods=["POST"])
 def create_recipe():
     form = NewRecipe()
     if form.validate_on_submit():
         data = form.data
         new_recipe = Recipe(userId=data["userId"],
-                                    dish_name=data["dish_name"],
-                                    ingredients=data["ingredients"],
-                                    instructions=data["instructions"],
-                                    photoUrl=data["photoUrl"])
+                            dish_name=data["dish_name"],
+                            ingredients=data["ingredients"],
+                            instructions=data["instructions"],
+                            photoUrl=data["photoUrl"])
         db.session.add(new_recipe)
         db.session.commit()
         return redirect("/feed")
     return "Uh-oh. There's something wrong here..."
+
+
+@recipe_routes.route('searched/<keyword>', methods=['GET'])
+def searched(keyword):
+    recipes = Recipe.query.filter(Recipe.dish_name.ilike(f'%{keyword}%')).all()
+    return {}
