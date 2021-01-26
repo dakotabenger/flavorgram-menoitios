74a3487d085fb8d6e039884a721b9cd2a697e5a8
Derek Nungesser <nungesserfilms@gmail.com>
2021-01-26 18:31:24 -0500

    finished form page, needs help connecting buttons

--- react-app/src/store/post.js
+++ react-app/src/store/post.js
@@ -2,4 +2,27 @@ import {fetch} from './csrf.js'
 
 const ADD_POST = 'recipes/ADD_POST'
 
-export const
+const addNewRecipe = recipe => ({
+    type: ADD_POST,
+    recipe,
+  });
+
+export const addRecipe = ({ dish_name, ingredients, instructions, photoUrl }) => async (dispatch) => {
+    const res = await fetch('/api/recipes/create_recipe', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            dish_name,
+            ingredients,
+            instructions,
+            photoUrl })
+    });
+
+    if(res.ok) {
+        const recipe = await res.json()
+        dispatch(addNewRecipe(recipe))
+        return recipe
+    }
+}
