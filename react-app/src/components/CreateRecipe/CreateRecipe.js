822d3586d9445036dc634da8a27f7f63b5e098f4
Dez <dez@dezs-mbp.attlocal.net>
2021-01-27 11:17:34 -0500

    integrated image upload to recipeform:

--- react-app/src/components/CreateRecipe/CreateRecipe.js
+++ react-app/src/components/CreateRecipe/CreateRecipe.js
@@ -1,61 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { addRecipe } from '../../store/post';
+import { useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { addRecipe } from "../../store/post";
 import * as sessionActions from "../../store/post";
-import './createrecipe.css'
+import "./createrecipe.css";
+import ImageGen from "../ImagePost/ImageGen";
 
 function CreateRecipe() {
-    const [dish_name, setDish_Name] = useState('');
-    const [ingredients, setIngredients] = useState('');
-    const [instructions, setInstructions] = useState('');
-    const [photoUrl, setPhotoUrl] = useState('');
+  const [dish_name, setDish_Name] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const payload = {
-        dish_name,
-        ingredients,
-        instructions,
-        photoUrl
-        };
-
-        const createdRecipe = dispatch(sessionActions.addRecipe(payload))
-
-        if (createdRecipe) return <Redirect to="/" />;
+    const payload = {
+      dish_name,
+      ingredients,
+      instructions,
+      photoUrl,
     };
 
-    const handleCancel = () => {
-        return <Redirect to="/" />;
-    };
+    const createdRecipe = dispatch(sessionActions.addRecipe(payload));
 
+    if (createdRecipe) return <Redirect to="/" />;
+  };
 
-    return(
-        <>
-        <h1 className="pagename">Create a New Recipe</h1>
-            <section>
-                <form onSubmit={handleSubmit}>
-                    <div className="form_container">
-                        <h2>Title</h2>
-                        <input type="text" className="title"/>
-                        <h2>Ingredients</h2>
-                        <textarea type="text" className="ingredients"/>
-                        <h2>Instructions</h2>
-                        <textarea type="text" className="instructions"/>
-                        <h2>Photo Url</h2>
-                        <input type="text" className="photourl"/>
-                        <button type="submit" className="submit_button">Create Recipe</button>
-                        <button type="button" className="cancel_button" onClick={handleCancel}>Cancel</button>
-                    </div>
-                </form>
-            </section>
-        </>
-    )
+  const handleCancel = () => {
+    return <Redirect to="/" />;
+  };
+
+  return (
+    <>
+      <h1 className="pagename">Create a New Recipe</h1>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div className="form_container">
+            <ImageGen />
+            <h2>Title</h2>
+            <input type="text" className="title" />
+            <h2>Ingredients</h2>
+            <textarea type="text" className="ingredients" />
+            <h2>Instructions</h2>
+            <textarea type="text" className="instructions" />
+            <h2>Photo Url</h2>
+            <input type="text" className="photourl" />
+            <button type="submit" className="submit_button">
+              Create Recipe
+            </button>
+            <button
+              type="button"
+              className="cancel_button"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default CreateRecipe
+export default CreateRecipe;
