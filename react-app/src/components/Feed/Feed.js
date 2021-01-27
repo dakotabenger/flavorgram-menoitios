125ad85280c4c8b01e1d8e91deaccbda3775664c
Dez <dez@dezs-mbp.attlocal.net>
2021-01-27 13:44:46 -0500

    post feed setup

--- react-app/src/components/Feed/Feed.js
+++ react-app/src/components/Feed/Feed.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./post";
 import "./Feed.css";
+import { authenticate } from "../../services/auth";
+
 const Feed = () => {
+  const [recipe, setRecipe] = useState([]);
+  const [loaded, setLoaded] = useState(true); //set to false once logged in
+  const [users, setUsers] = useState({});
+  const [myUserId, setMyUserId] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let res = await fetch("/api/recipes/");
+      res = await res.json();
+      setRecipe(res.Recipe);
+      setUsers(res.users);
+      setLoaded(true);
+      setMyUserId((await authenticate()).id);
+    })();
+  }, []);
   return (
-    <>
-      <Post />
-    </>
+    loaded && (
+      <div className="post-container">
+        <Post />
+      </div>
+    )
   );
 };
 
