ccd68597cd8c0470a3c86e296bd57358918d038e
ramses <ramses.romero.jr@gmail.com>
2021-01-26 19:56:03 -0500

    got search results to show image

--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -10,7 +10,10 @@ const SearchResults = () => {
         if(results.recipes.length > 0){
             const resultRecipes = results.recipes
             recipes = resultRecipes.map(recipe => {
-                return <div key={recipe.id}> {recipe.id} </div>
+                return <div key={recipe.id}>
+                    <h2>{recipe.id}</h2>
+                    <img src={`${recipe.photo_url}`}/>
+                 </div>
             })
         }
     }
