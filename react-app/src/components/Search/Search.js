48a7fe16e46c7745f98151792ed62d8dcf603575
Dez <dez@dezs-mbp.attlocal.net>
2021-01-28 17:23:48 -0500

    search pop ups

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import "../NavBar/NavBar.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useHistory } from "react-router-dom";
-import { getSearchResults, removeSearchResults } from "../../store/search";
+import * as searchActions from "../../store/search";
 
 const StyledInput = styled.input`
   height: 35px;
@@ -12,46 +12,28 @@ const StyledInput = styled.input`
   text-align: center;
 `;
 const Search = () => {
-  const [term, setTerm] = useState("");
-  // const searchResults = useSelector((state) => state.search.results);
+  const [search, setSearch] = useState("");
+  const searchResults = useSelector((state) => state.search.results);
   const dispatch = useDispatch();
   const history = useHistory();
+  const onSearch = (e) => {
+    e.preventDefault();
 
-  useEffect(() => {
-    const setSearchResults = async () => {
-      if (term !== "") {
-        await dispatch(getSearchResults(term));
-      } else {
-        dispatch(removeSearchResults());
-      }
-    };
-
-    setSearchResults();
-  }, [term]);
-
-  const handleChange = (e) => {
-    setTerm(e.target.value);
+    return dispatch(searchActions.search(search));
   };
 
-  // const onSearch = (e) => {
-  //   e.preventDefault();
+  if (searchResults) {
+    history.push("/search-results");
+  }
 
-  //   return dispatch(searchActions.search(term));
-  // };
-
-  // if (searchResults) {
-  //   history.push("/search-results");
-  // }
-  // onSubmit = { onSearch };
   return (
     <div>
-      <form>
+      <form onSubmit={onSearch}>
         <StyledInput
           type="text"
-          value={term}
           className="searchbar fas fa-search"
           placeholder="Search Recipes..."
-          onChange={handleChange}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </form>
     </div>
--- react-app/src/store/search.js
+++ react-app/src/store/search.js
@@ -1,52 +1,31 @@
 import { fetch } from "./csrf.js";
 
 //keys
-const SET_SEARCH_RESULTS = "searched/findRecipe";
-const REMOVE_SEARCH_RESULTS = "searched/removeRecipe";
+const FIND_RECIPE = "searched/findRecipe";
+
 //action to set searched recipes
-const setSearchResults = (searchResults) => ({
-  type: SET_SEARCH_RESULTS,
-  payload: searchResults,
+const setRecipe = (recipes) => ({
+  type: FIND_RECIPE,
+  recipes: recipes,
 });
 
-export const removeSearchResults = () => {
-  return {
-    type: REMOVE_SEARCH_RESULTS,
-  };
-};
-
 //thunk to fetch searched recipes then dispatch setRecipe action
-// export const getSearchResults = (term) => async (dispatch) => {
-//   // console.log(searchedRecipe)
-//   const res = await fetch(`api/recipes/searched/${term}`);
-//   dispatch(setRecipe(res.data));
-//   return res;
-// };
-
-export const getSearchResults = (term) => async (dispatch) => {
-  const res = await fetch(`/api/recipes/searched/${term}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ term }),
-  });
-
-  const searchResults = res.data;
-  dispatch(setSearchResults({ ...searchResults, term }));
+export const search = (searchedRecipe) => async (dispatch) => {
+  // console.log(searchedRecipe)
+  const res = await fetch(`api/recipes/searched/${searchedRecipe}`);
+  dispatch(setRecipe(res.data));
+  return res;
 };
 
-const searchReducer = (state = {}, action) => {
+function reducer(state = { results: null }, action) {
+  let newState;
   switch (action.type) {
-    case SET_SEARCH_RESULTS:
-      return {
-        ...state,
-        term: action.payload.term,
-      };
-    case REMOVE_SEARCH_RESULTS:
-      return { ...state, term: "" };
+    case FIND_RECIPE:
+      newState = Object.assign({}, state, { results: action.recipes });
+      return newState;
     default:
       return state;
   }
-};
-export default searchReducer;
+}
+
+export default reducer;
