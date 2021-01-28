c4ed478b43146f132b13002a60a5518760e7bef3
Dez <dez@dezs-mbp.attlocal.net>
2021-01-28 16:22:32 -0500

    search started and redux set

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -1,43 +1,61 @@
 import styled from "styled-components";
-import '../NavBar/NavBar.css'
-import React, {useState} from 'react';
+import "../NavBar/NavBar.css";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect,useHistory } from "react-router-dom";
-import * as searchActions from '../../store/search'
-
+import { Redirect, useHistory } from "react-router-dom";
+import { getSearchResults, removeSearchResults } from "../../store/search";
 
 const StyledInput = styled.input`
-  height:35px;
+  height: 35px;
   width: 500px;
   font-size: 30px;
   text-align: center;
-
-`
+`;
 const Search = () => {
-  const [search, setSearch] = useState('')
-  const searchResults = useSelector((state) => state.search.results);
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const onSearch = (e) => {
-    e.preventDefault();
+  const [term, setTerm] = useState("");
+  // const searchResults = useSelector((state) => state.search.results);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    return dispatch(searchActions.search(search))
-  }
+  useEffect(() => {
+    const setSearchResults = async () => {
+      if (term !== "") {
+        await dispatch(getSearchResults(term));
+      } else {
+        dispatch(removeSearchResults());
+      }
+    };
 
-  if(searchResults) {history.push("/search-results")}
+    setSearchResults();
+  }, [term]);
 
-    return (
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  // const onSearch = (e) => {
+  //   e.preventDefault();
+
+  //   return dispatch(searchActions.search(term));
+  // };
+
+  // if (searchResults) {
+  //   history.push("/search-results");
+  // }
+  // onSubmit = { onSearch };
+  return (
     <div>
-      <form onSubmit={onSearch}>
-      <StyledInput
-        type="text"
-        className="searchbar fas fa-search"
-        placeholder="Search Recipes..."
-        onChange={(e) => setSearch(e.target.value)}
+      <form>
+        <StyledInput
+          type="text"
+          value={term}
+          className="searchbar fas fa-search"
+          placeholder="Search Recipes..."
+          onChange={handleChange}
         />
       </form>
     </div>
-    )
-}
+  );
+};
 
 export default Search;
--- react-app/src/store/search.js
+++ react-app/src/store/search.js
@@ -1,31 +1,52 @@
-import { fetch } from './csrf.js';
+import { fetch } from "./csrf.js";
 
 //keys
-const FIND_RECIPE = 'searched/findRecipe'
-
+const SET_SEARCH_RESULTS = "searched/findRecipe";
+const REMOVE_SEARCH_RESULTS = "searched/removeRecipe";
 //action to set searched recipes
-const setRecipe = (recipes) => ({
-    type:FIND_RECIPE,
-    recipes: recipes
-})
+const setSearchResults = (searchResults) => ({
+  type: SET_SEARCH_RESULTS,
+  payload: searchResults,
+});
+
+export const removeSearchResults = () => {
+  return {
+    type: REMOVE_SEARCH_RESULTS,
+  };
+};
 
 //thunk to fetch searched recipes then dispatch setRecipe action
-export const search = (searchedRecipe) => async (dispatch) => {
-    // console.log(searchedRecipe)
-    const res = await fetch(`api/recipes/searched/${searchedRecipe}`)
-    dispatch(setRecipe(res.data))
-    return res
-}
+// export const getSearchResults = (term) => async (dispatch) => {
+//   // console.log(searchedRecipe)
+//   const res = await fetch(`api/recipes/searched/${term}`);
+//   dispatch(setRecipe(res.data));
+//   return res;
+// };
 
-function reducer(state = {results: null}, action){
-    let newState;
-    switch(action.type){
-        case FIND_RECIPE:
-            newState = Object.assign({}, state, {results: action.recipes});
-            return newState;
-        default:
-            return state;
-    }
-}
+export const getSearchResults = (term) => async (dispatch) => {
+  const res = await fetch(`/api/recipes/searched/${term}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ term }),
+  });
 
-export default reducer;
+  const searchResults = res.data;
+  dispatch(setSearchResults({ ...searchResults, term }));
+};
+
+const searchReducer = (state = {}, action) => {
+  switch (action.type) {
+    case SET_SEARCH_RESULTS:
+      return {
+        ...state,
+        term: action.payload.term,
+      };
+    case REMOVE_SEARCH_RESULTS:
+      return { ...state, term: "" };
+    default:
+      return state;
+  }
+};
+export default searchReducer;
