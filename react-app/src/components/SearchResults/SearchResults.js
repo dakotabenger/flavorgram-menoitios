cf52f66d9c91040e80013e7cc22d09ace7c4010f
ramses <ramses.romero.jr@gmail.com>
2021-01-29 19:32:40 -0500

    got search result to load user poster too

--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory, NavLink } from "react-router-dom";
 
 const SearchResults = () => {
   const results = useSelector((state) => state.search.results);
+  const history = useHistory();
 
   let recipes = <h1>Sorry No Recipes Match That Query</h1>;
   if (results) {
@@ -10,12 +12,34 @@ const SearchResults = () => {
       const resultRecipes = results.recipes;
       recipes = resultRecipes.map((recipe) => {
         return (
-          <div className="search-main__container" key={recipe.id}>
-            <div className="one-searched-container">
+          <div className="post-main__container">
+            <div className="one-post-container">
+            <div className="post-user-info">
+          <img
+            className="post-profile-pic"
+            src={recipe.user.avatarUrl}
+            alt={`profile pic of ${recipe.user.username}`}
+          />
+          <div>
+            <NavLink
+              className="post-author-name"
+              to={`/users/${recipe.user.username}`}
+            >
+              {recipe.user.username}
+            </NavLink>
+          </div>
+          </div>
               <h2 className="container__title">{recipe.dish_name}</h2>
-              {/* <p>{recipe.instructions}</p>
-              <p>{recipe.ingredients}</p> */}
-              <img className="image-container" src={`${recipe.photoUrl}`} />
+              <div className="feed-post-img-container">
+          <img
+            className="actual-image"
+            src={recipe.photoUrl}
+            alt={recipe.dish_name}
+            onClick={(e) => {
+              history.push(`/recipes/${recipe.id}`);
+            }}
+          />
+        </div>
             </div>
           </div>
         );
