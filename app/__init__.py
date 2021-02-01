0cf4c65426987dcdd4576c20fa3a92281c5eac3c
Dez <dez@dezs-mbp.attlocal.net>
2021-02-01 10:29:21 -0500

    favicon change

--- app/__init__.py
+++ app/__init__.py
@@ -75,6 +75,6 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path == 'knifork.png':
+        return app.send_static_file('knifork.png')
     return app.send_static_file('index.html')
